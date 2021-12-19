7ec4532d5a94bfd529088a0cd282a1708de70b24
Felipe Munevar <32135161+felipemunevarn@users.noreply.github.com>
2021-12-19 01:59:05 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const { ReclamosModel } = require("./models/Reclamos")
 const NUM_TRAN = "61b94da73eec120a384b90ef"
 const NUM_RECL = "61b94dd51ff22b3ee1283ab2"
 
-const URL = `mongodb+srv://${process.env.REACT_APP_USER}:${process.env.REACT_APP_PASSWORD}@banagrario.57kdk.mongodb.net/test?retryWrites=true`
+const URL = `mongodb+srv://${process.env.REACT_APP_USER}:${process.env.REACT_APP_PASSWORD}@banagrario.57kdk.mongodb.net/${process.env.REACT_APP_DB}?retryWrites=true&w=majority`
 const app = express()
 
 app.use(express.json())
